c68d40e45ec665c573f9dd8cc8e0bfb13730e688
NoobPete <peteretep@live.dk>
2019-04-28 02:04:41 +0200

    Added ammoText to prefab, more work on sound system

---
+++ Assets/Sounds/AudioManeger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManeger : MonoBehaviour
+{
+    public static AudioManeger main;
+    public List<AudioClip> ShootSounds = new List<AudioClip>();
+    public List<AudioClip> StepSound = new List<AudioClip>();
+    public List<AudioClip> ReloadSound = new List<AudioClip>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        main = this;
+    }
+
+    public void Play(AudioClip a, Vector3 pos)
+    {
+        AudioSource.PlayClipAtPoint(a, pos);
+    }
+
+    public void Play(List<AudioClip> a, Vector3 pos)
+    {
+        AudioSource.PlayClipAtPoint(a[UnityEngine.Random.Range(0, a.Count)] , pos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
