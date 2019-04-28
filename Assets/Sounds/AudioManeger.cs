b5ebbfc2910ba46340e977d03faeb38d1f484a6c
NoobPete <peteretep@live.dk>
2019-04-28 12:11:14 +0200

    Player step sounds and disable line from weapon by deafult

--- Assets/Sounds/AudioManeger.cs
+++ Assets/Sounds/AudioManeger.cs
@@ -8,6 +8,7 @@ public class AudioManeger : MonoBehaviour
     public List<AudioClip> ShootSounds = new List<AudioClip>();
     public List<AudioClip> StepSound = new List<AudioClip>();
     public List<AudioClip> ReloadSound = new List<AudioClip>();
+    public List<AudioClip> WallHitSound = new List<AudioClip>();
 
     // Start is called before the first frame update
     void Start()
